d9ed7c0f0e5e35e112003caed7fc798d643ffa91
antocreadev <anthonymenghi@icloud.com>
2025-02-06 11:37:00 +0100

    Initial Commit

---
+++ antocreadev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  antocreadev
+//
+//  Created by antocreadev on 06/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ antocreadevTests/antocreadevTests.swift
@@ -0,0 +1,17 @@
+//
+//  antocreadevTests.swift
+//  antocreadevTests
+//
+//  Created by antocreadev on 06/02/2025.
+//
+
+import Testing
+@testable import antocreadev
+
+struct antocreadevTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
